27621bd043c07a405ae637aeb5c3721238fea51a
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-21 13:16:08 +0300

    CRUD

--- index.js
+++ index.js
@@ -26,7 +26,27 @@ const database = [
 ];
 
 app.get('/get', (req, res) => {
-  res.json(database);
+  res.json(database).status(200);
 });
-
+app.post('/post', (req, res) => {
+  database.push({
+    id: 5,
+    name: 'john doe',
+    age: 19,
+  });
+  res.json(database).status(201);
+});
+app.delete('/delete', (req, res) => {
+  database.pop();
+  res.json(database).status(201);
+});
+app.put('/put', (req, res) => {
+  database.map((user, index) => {
+    if (user.id === 1) {
+      database[index] = { id: 123, name: 'Johns', age: 34 };
+    }
+    return user;
+  });
+  res.json(database).status(201);
+});
 app.listen(3000, () => console.log('Server has been started'));
